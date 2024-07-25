16617ba6bd86fa02c5b632991cfa84b190a9bd9a
Maria <maryallbond@gmail.com>
2024-07-25 10:15:46 +0300

    redact swiper of popular films

--- src/js/main/swiper.js
+++ src/js/main/swiper.js
@@ -3,33 +3,30 @@ import "swiper/css";
 import "swiper/css/bundle";
 
 const swiper = new Swiper(".swiper", {
-  navigation : {
+  navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   loop: true,
-  touchRation: 1.5,
-  grabCursor: true,
   keyboard: {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
   },
   mousewheel: {
-    sensitivity: 0.5,
+    sensitivity: 0.35,
     eventsTarget: ".swiper",
   },
-  initialSlide: 1,
-  centeredSlides: true,
   autoplay: {
-    delay: 2500,
+    delay: 2000,
     disableOnInteraction: true,
   },
-  speed: 700,
+  speed: 500,
   spaceBetween: "50px",
   observeSlideChildren: true,
   observer: true,
   observeParents: true,
   slidesPerView: 4,
   slidesPerGroup: 2,
+  freeMode: true,
 });
